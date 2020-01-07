3bdfd6a2b3b80c6ecf1976c9f36c9607e18689d0
Ro <891220155715@mail.ru>
2020-01-07 22:39:12 +0300

    1

--- CurseWorkOpenGLRogoznev/Form1.Designer.cs
+++ CurseWorkOpenGLRogoznev/Form1.Designer.cs
@@ -33,6 +33,14 @@
             this.FigureChanger = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.RenderTimer = new System.Windows.Forms.Timer(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Ylabel = new System.Windows.Forms.Label();
+            this.Xlabel = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
             this.SuspendLayout();
             // 
             // AnT
@@ -77,17 +85,81 @@
             // RenderTimer
             // 
             this.RenderTimer.Interval = 25;
+            this.RenderTimer.Tick += new System.EventHandler(this.RenderTimer_Tick);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(592, 107);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(196, 45);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(592, 170);
+            this.trackBar2.Maximum = 100;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(196, 45);
+            this.trackBar2.TabIndex = 4;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(592, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(210, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Координата точки схождения на оси ОХ";
+            // 
+            // Ylabel
+            // 
+            this.Ylabel.AutoSize = true;
+            this.Ylabel.Location = new System.Drawing.Point(592, 154);
+            this.Ylabel.Name = "Ylabel";
+            this.Ylabel.Size = new System.Drawing.Size(35, 13);
+            this.Ylabel.TabIndex = 6;
+            this.Ylabel.Text = "label3";
+            // 
+            // Xlabel
+            // 
+            this.Xlabel.AutoSize = true;
+            this.Xlabel.Location = new System.Drawing.Point(592, 91);
+            this.Xlabel.Name = "Xlabel";
+            this.Xlabel.Size = new System.Drawing.Size(35, 13);
+            this.Xlabel.TabIndex = 8;
+            this.Xlabel.Text = "label5";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(589, 131);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(211, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Координата точки схождения на оси ОУ";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Xlabel);
+            this.Controls.Add(this.Ylabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.FigureChanger);
             this.Controls.Add(this.AnT);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Курсовая работа  по Компьютерной графике";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -99,6 +171,12 @@
         private System.Windows.Forms.ComboBox FigureChanger;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer RenderTimer;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Xlabel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label Ylabel;
     }
 }
 
--- CurseWorkOpenGLRogoznev/Form1.cs
+++ CurseWorkOpenGLRogoznev/Form1.cs
@@ -7,16 +7,99 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tao.FreeGlut;
+using Tao.OpenGl;
+using Tao.Platform;
 
 namespace CurseWorkOpenGLRogoznev
 {
     public partial class Form1 : Form
     {
+        double a = 0, b = 0, c = -5;
+        int vanishingPointOX = 0, vanishingPointOY = 0;
+        float xVanishingPoint=0.0f, yVanishingPoint=0.0f;
+
+
         public Form1()
         {
             InitializeComponent();
+            AnT.InitializeContexts();
         }
 
-        
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            vanishingPointOX = trackBar1.Value;
+            Xlabel.Text =vanishingPointOX.ToString()+";0;0;";
+            xVanishingPoint = (float)vanishingPointOX/100.0f; //100 для проверки, выяснить сколько надо брать
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            vanishingPointOY = trackBar2.Value;
+            Ylabel.Text = "0;" + vanishingPointOY.ToString()+";0";
+            yVanishingPoint = (float)vanishingPointOY/100.0f;//100 для проверки, выяснить сколько надо брать
+        }
+
+        private void RenderTimer_Tick(object sender, EventArgs e)
+        {
+            Draw();
+        }
+
+        private void Draw()
+        {
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            Gl.glLoadIdentity();
+            float[] m = new float[] { 1, 0, 0, xVanishingPoint, 0, 1, 0, yVanishingPoint, 0, 0, 1, 0, 0, 0, 0, 1 };
+            Gl.glLoadMatrixf(m);
+
+            Glu.gluPerspective(45, (float)AnT.Width / (float)AnT.Height, 2, 200);
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+            Gl.glLoadIdentity();
+
+            Gl.glEnable(Gl.GL_DEPTH_TEST);
+            Gl.glEnable(Gl.GL_LIGHTING);
+            Gl.glEnable(Gl.GL_LIGHT0);
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
+            Gl.glClearColor(255, 255, 255, 1);
+            Gl.glLoadIdentity();
+            Gl.glPushMatrix();
+            Gl.glTranslated(a, b, c);
+            //Gl.glRotated(d, os_x, os_y, os_z);
+            //Gl.glScaled(zoom, zoom, zoom);
+
+            switch (FigureChanger.SelectedIndex)//тетраэдр гексаэдр октаэдр
+            {
+                case 0:
+                    {
+                        Glut.glutWireTetrahedron();
+                    }
+                    break;
+                case 1:
+                    {
+                        Glut.glutWireCube(1);
+                    }
+                    break;
+                case 2:
+                    {
+                        Glut.glutWireOctahedron();
+                    }
+                    break;
+
+            }
+            Gl.glPopMatrix();
+            Gl.glFlush();
+            AnT.Invalidate();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Glut.glutInit();
+            Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_DOUBLE);
+            Gl.glClearColor(255, 255, 255, 1);
+            Gl.glViewport(0, 0, AnT.Width, AnT.Height);
+            FigureChanger.SelectedIndex = 0;
+            RenderTimer.Start();
+        }
     }
+
 }
