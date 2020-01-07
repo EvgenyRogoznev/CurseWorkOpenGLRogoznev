84311e2ea73e28b8627533894231acefb3ce584d
Ro <891220155715@mail.ru>
2020-01-07 21:11:42 +0300

    Добавьте файлы проекта.

---
+++ CurseWorkOpenGLRogoznev/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace CurseWorkOpenGLRogoznev
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AnT = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.FigureChanger = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.RenderTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // AnT
+            // 
+            this.AnT.AccumBits = ((byte)(0));
+            this.AnT.AutoCheckErrors = false;
+            this.AnT.AutoFinish = false;
+            this.AnT.AutoMakeCurrent = true;
+            this.AnT.AutoSwapBuffers = true;
+            this.AnT.BackColor = System.Drawing.Color.Black;
+            this.AnT.ColorBits = ((byte)(32));
+            this.AnT.DepthBits = ((byte)(16));
+            this.AnT.Location = new System.Drawing.Point(12, 12);
+            this.AnT.Name = "AnT";
+            this.AnT.Size = new System.Drawing.Size(574, 426);
+            this.AnT.StencilBits = ((byte)(0));
+            this.AnT.TabIndex = 0;
+            // 
+            // FigureChanger
+            // 
+            this.FigureChanger.DisplayMember = "0";
+            this.FigureChanger.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.FigureChanger.FormattingEnabled = true;
+            this.FigureChanger.Items.AddRange(new object[] {
+            "Тетраэдр",
+            "Октаэдр",
+            "Гексаэдр"});
+            this.FigureChanger.Location = new System.Drawing.Point(592, 33);
+            this.FigureChanger.Name = "FigureChanger";
+            this.FigureChanger.Size = new System.Drawing.Size(121, 21);
+            this.FigureChanger.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(589, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Выбор фигуры";
+            // 
+            // RenderTimer
+            // 
+            this.RenderTimer.Interval = 25;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.FigureChanger);
+            this.Controls.Add(this.AnT);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl AnT;
+        private System.Windows.Forms.ComboBox FigureChanger;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer RenderTimer;
+    }
+}
+
